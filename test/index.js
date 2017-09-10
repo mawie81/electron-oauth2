c3cd8b8e2dc5a2c02351ddb45bc1f6961f1d9ca8
Vishal Isharani <isharanivishal@gmail.com>
2017-09-10 10:35:13 +0530

    preparing for npm

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-xdescribe('electron/oauth2', function () {
+xdescribe('electron/oauth', function () {
   it('should not throw an error', function () {
     require('../index');
   });
